cf0762f3acb7fa587efaa97f7c752e575595df02
Iram Ismail <iram.8859@gmail.com>
2019-04-28 12:20:09 +0530

    comitAll

---
+++ tds/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tds.Startup))]
+namespace tds
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
