f143af3a0047a455b74de1c239e463e80dd7e2b3
wajahat Nabi <wajahatnabi90@gmail.com>
2019-04-30 14:59:52 +0530

    changes in identityModels

--- tds/Models/IdentityModels.cs
+++ tds/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 
 namespace tds.Models
 {
@@ -23,15 +24,51 @@ namespace tds.Models
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            Database.SetInitializer<ApplicationDbContext>(new MyDBInitializer());
         }
+        public class MyDBInitializer : CreateDatabaseIfNotExists<ApplicationDbContext>
+        {
+            protected override void Seed(ApplicationDbContext context)
+            {
+                // Initialize default identity roles
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                // RoleTypes is a class containing constant string values for different roles
+                List<IdentityRole> identityRoles = new List<IdentityRole>();
+                identityRoles.Add(new IdentityRole() { Name = "Admin" });
+                foreach (IdentityRole role in identityRoles)
+                {
+                    manager.Create(role);
+                }
+
+                // Initialize default user
+                var ustore = new UserStore<ApplicationUser>(context);
+                var umanager = new UserManager<ApplicationUser>(ustore);
+                var admin = new ApplicationUser {Email="admin@admin.com",PhoneNumber="123456789",UserName="admin"};
+                umanager.Create(admin, "123456");
+                umanager.AddToRole(admin.Id, "Admin");
+
+                // Add code to initialize context tables
+
+                base.Seed(context);
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
 
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
         }
-        public DbSet<Models.Deductor> Deductor { get; set; }
-        public DbSet<Models.Contractor> Contractor { get; set; }
-        public DbSet<Models.Tax> Tax { get; set; }
-        public DbSet<Models.Transaction> Transaction { get; set; }
     }
 }
